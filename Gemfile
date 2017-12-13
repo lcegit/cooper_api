702681c05f83a63cfdefe458689816d63d2fb2ff
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-13 13:33:33 +0100

    Setup Request spec (API testing framework)

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
+
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -9,21 +9,17 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module CooperApi
   class Application < Rails::Application
     config.api_only = true
-      config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins '*'
-          resource '*', headers: :any, methods: [:get, :post, :put, :delete]
-        end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete]
       end
     end
   end
+end
