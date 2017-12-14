3007dcf89d6b5ef6d4dd878607047e3ec23ff92a
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-14 09:37:35 +0100

    Adds PerformanceData model - a method to store historical data for each user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,5 +51,9 @@ RSpec.describe User, type: :model do
       it { is_expected.to allow_value(email).for(:email) }
     end
   end
-end
+
+    describe 'Relations' do
+      it { is_expected.to have_many :performance_data }
+    end
+  end
 end
